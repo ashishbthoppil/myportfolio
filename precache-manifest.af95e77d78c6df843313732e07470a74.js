8f7b986ed11061a0c439eff787df216617daeed5
ashishbthoppil <ashishbabythoppil@gmail.com>
2020-09-29 18:08:10 +0530

    Updates

---
+++ precache-manifest.af95e77d78c6df843313732e07470a74.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a02cc44aa2f341b9ef31ca5e6fb8dc9b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c895801ead66de4f756e",
+    "url": "/portfolio/static/css/main.06d611a2.chunk.css"
+  },
+  {
+    "revision": "e50552cd716e4b64858f",
+    "url": "/portfolio/static/js/2.823564b5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.823564b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c895801ead66de4f756e",
+    "url": "/portfolio/static/js/main.9a4ee3cd.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "1453114f769d6929538ed47f04170396",
+    "url": "/portfolio/static/media/anonymous-text-chat.1453114f.jpg"
+  },
+  {
+    "revision": "0b411e121b1fda0f537f294007775c67",
+    "url": "/portfolio/static/media/me.0b411e12.jpg"
+  },
+  {
+    "revision": "96a0c5f7fbe82ec8b280bc9795d2a17f",
+    "url": "/portfolio/static/media/mini-social.96a0c5f7.jpg"
+  },
+  {
+    "revision": "fa7448ac4b4db27dca70be1ffb7d69de",
+    "url": "/portfolio/static/media/radiology.fa7448ac.jpg"
+  },
+  {
+    "revision": "81bcc9212b1adb4bb7a8e294b8645f63",
+    "url": "/portfolio/static/media/training.81bcc921.jpg"
+  }
+]);
